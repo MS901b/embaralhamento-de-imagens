a1954c240b7f553fe6483577c4e9ab94e02812ea
Yukine28 <c195727@dac.unicamp.br>
2020-01-07 22:07:00 -0300

    Alterado letras e links

--- embaralhamento/scripts/abertura.js
+++ embaralhamento/scripts/abertura.js
@@ -4,8 +4,8 @@ Event.observe(window, 'load', function ()
 {
 	PopupFichaTecnica = new FichaTecnica('Ficha técnica da unidade', FichaUnidade, 'ficha_tecnica_unidade');
 	PopupFichaProjeto = new FichaTecnica('Ficha técnica do projeto', FichaProjeto, 'ficha_tecnica_projeto');
-	
-     // analisaAmbiente();
+
+     analisaAmbiente();
 });
 
 
@@ -17,15 +17,15 @@ var FichaTecnica = Class.create({
 			className: 'ficha_tecnica',
 			style: 'width:'+(this.largura-20)+'px; height: '+(this.altura-20)+'px'
 		})
-		
-		
+
+
 		this.titulo = new Element('h2');
 		this.titulo.update(titulo);
-		
+
 		this.texto = new Element('div', {className: 'texto'});
-		
+
 		this.texto.insert(this.titulo);
-		
+
 		for (var a = 0; a < vetor.length; a++)
 		{
 
@@ -36,10 +36,10 @@ var FichaTecnica = Class.create({
 			this.texto.insert(subtitulo);
 			this.texto.insert(conteudo);
 		}
-		
+
 		this.conteudo.insert(this.texto);
 
-		this.popup = new PopupCallback($(pai), this.conteudo, ['seta_baixo', 'esquerda'], 20, function() {}, [{fechar: 'Fechar'}]);	
+		this.popup = new PopupCallback($(pai), this.conteudo, ['seta_baixo', 'esquerda'], 20, function() {}, [{fechar: 'Fechar'}]);
 	}
 });
 
@@ -48,27 +48,27 @@ function analisaAmbiente()
 	var flashRequiredMajorVersion = 9.0;
 	var flashRequiredMinorVersion = 0;
 	var flashRequiredRevision = 0;
-	
+
 	var javaRequiredVersion="1.4+";
 	var hasReqestedVersion = DetectFlashVer(flashRequiredMajorVersion, flashRequiredMinorVersion, flashRequiredRevision);
-	
+
 	var protocol = window.location.protocol;
-	
+
 	if (protocol != 'http:' && protocol != 'https:')
 	{
-		var conteudo = 
+		var conteudo =
 			'<p><strong>Erro de execução – Não abra esse arquivo html diretamente.</strong></p>'
 			+ '<p>Este software deve ser visualizado de um servidor na internet ou utilizando o programa AcessarProdutos fornecido com ele.</p>'
 			+ '<p><a id="fechar_desesperado" style="float:right; href="#">Fechar</a></p>'
 			+ '<br class="limpador" />';
-		
+
 		var tmp = new PopupDesesperados(conteudo, 30);
 
 		$('iniciar_software').remove();
 		return;
 	}
-	
-	
+
+
 	if (!(BrowserDetect.browser=='Firefox' && BrowserDetect.version>=3))
 	{
 		if (!(BrowserDetect.browser=='Internet Explorer' && BrowserDetect.version>=7))
@@ -79,9 +79,9 @@ function analisaAmbiente()
 			var tmp = new PopupDesesperados(conteudo, 20);
 			return;
 		}
-	
+
 	}
-	
+
 	if (!hasReqestedVersion)
 	{
 		var conteudo = '<p><strong>Erro de compatibilidade – Adobe Flash Player</strong></p><p>A versão mínima necessária do plugin para usar este software é <strong><em>Adobe Flash Player '+flashRequiredMajorVersion+'</em></strong>.</p>';
@@ -90,7 +90,7 @@ function analisaAmbiente()
 		var tmp = new PopupDesesperados(conteudo, 20);
 		return;
 	}
-	
+
 	if (!deployJava.versionCheck(javaRequiredVersion))
 	{
 		var conteudo = '<p><strong>Erro de compatibilidade – Java Virtual Machine</strong></p><p>A versão mínima necessária da Java Virtual Machine para usar este software é <strong><em>'+javaRequiredVersion+'</em></strong>.</p>';
