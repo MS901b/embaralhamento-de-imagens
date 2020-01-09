11f6608a6d410d3db852db991885b24c6634605b
Yukine28 <c195727@dac.unicamp.br>
2020-01-09 01:27:05 -0300

    corrigido popup

--- embaralhamento/scripts/abertura.js
+++ embaralhamento/scripts/abertura.js
@@ -45,12 +45,11 @@ var FichaTecnica = Class.create({
 
 function analisaAmbiente()
 {
-	var flashRequiredMajorVersion = 9.0;
+	/*var flashRequiredMajorVersion = 9.0;
 	var flashRequiredMinorVersion = 0;
 	var flashRequiredRevision = 0;
-
 	var javaRequiredVersion="1.4+";
-	var hasReqestedVersion = DetectFlashVer(flashRequiredMajorVersion, flashRequiredMinorVersion, flashRequiredRevision);
+	var hasReqestedVersion = DetectFlashVer(flashRequiredMajorVersion, flashRequiredMinorVersion, flashRequiredRevision);*/
 
 	var protocol = window.location.protocol;
 
@@ -69,20 +68,20 @@ function analisaAmbiente()
 	}
 
 
-	if (!(BrowserDetect.browser=='Firefox' && BrowserDetect.version>=3))
+	if (!(BrowserDetect.browser=='Firefox' && BrowserDetect.version>=68))
 	{
-		if (!(BrowserDetect.browser=='Internet Explorer' && BrowserDetect.version>=7))
+		if (!(BrowserDetect.browser=='Chrome' && BrowserDetect.version>=75))
 		{
-			var conteudo = '<p><strong>Erro de compatibilidade – Navegador</strong></p><p>O seu navegador não é compatível com este software, para usar este software você precisa ter um dos seguintes navegadores de internet: </p><p>1. Firefox 3.0+ (mais recomendado)<br />2. Internet Explorer 7.0+';
+			var conteudo = '<p><strong>Erro de compatibilidade – Navegador</strong></p><p>O seu navegador não é compatível com este software, para usar este software você precisa ter um dos seguintes navegadores de internet: </p><p>1. Chrome 75+ (mais recomendado)<br />2. Firefox 68.0+';
 			conteudo += '<p style="float:right;"><a href="detect.html">Ver relatório de compatibilidade completo</a></p><br class="limpador" />';
-			conteudo += '<p><a id="fechar_desesperado" style="float:right; href="#">Fechar</a><a id="continuar_mesmo_assim" style="float:right; margin-right:10px;"" href="#">Continuar mesmo assim</a><a style="float:right; margin-right:10px;" href="http://www.mozilla.com/firefox">Instalar o Firefox</a></p><br class="limpador" />';
+			conteudo += '<p><a id="fechar_desesperado" style="float:right; href="#">Fechar</a><a id="continuar_mesmo_assim" style="float:right; margin-right:10px;"" href="#">Continuar mesmo assim</a><a style="float:right; margin-right:10px;" href="https://www.google.pt/intl/pt-PT/chrome/?brand=CHBD&gclid=EAIaIQobChMIteHJlKP15gIVkAyRCh1UAwU-EAAYASAAEgJUQPD_BwE&gclsrc=aw.ds">Instalar o Chrome</a></p><br class="limpador" />';
 			var tmp = new PopupDesesperados(conteudo, 20);
 			return;
 		}
 
 	}
-
-	if (!hasReqestedVersion)
+	//Flash e java não são mais necessarios
+	/*if (!hasReqestedVersion)
 	{
 		var conteudo = '<p><strong>Erro de compatibilidade – Adobe Flash Player</strong></p><p>A versão mínima necessária do plugin para usar este software é <strong><em>Adobe Flash Player '+flashRequiredMajorVersion+'</em></strong>.</p>';
 		conteudo += '<p style="float:right;"><a href="detect.html">Ver relatório de compatibilidade completo</a></p><br class="limpador" />';
@@ -98,5 +97,5 @@ function analisaAmbiente()
 		conteudo += '<p><a id="fechar_desesperado" style="float:right; href="#">Fechar</a><a id="continuar_mesmo_assim" style="float:right; margin-right:10px;"" href="#">Continuar mesmo assim</a><a style="float:right; margin-right:10px;" href="http://www.java.com">Instalar Java VM</a></p><br class="limpador" />';
 		var tmp = new PopupDesesperados(conteudo, 20);
 		return;
-	}
+	}*/
 }
